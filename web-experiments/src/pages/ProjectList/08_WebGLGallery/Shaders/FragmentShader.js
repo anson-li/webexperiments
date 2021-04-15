df997716f1a45ed8585b1009ba734885e48a6d22
Anson Li <hello@ansonli.ca>
2021-04-14 20:55:18 -0600

    Updated FragShader to light up the center of the page (have to adjust for multiview)

--- web-experiments/src/pages/ProjectList/08_WebGLGallery/Shaders/FragmentShader.js
+++ web-experiments/src/pages/ProjectList/08_WebGLGallery/Shaders/FragmentShader.js
@@ -8,7 +8,16 @@ const FragmentShader = `
     
   void main() {
     vec2 textureCoord = vTextureCoord;    
-    gl_FragColor = texture2D(uSampler0, vTextureCoord);
+    vec4 finalColor = texture2D(uSampler0, vTextureCoord);
+    float f = 0.5;
+    float r = finalColor.r;
+    float g = finalColor.g;
+    float b = finalColor.b;
+    float L = 0.3 * r + 0.6 * g + 0.1 * b; // desaturation algorithm
+    finalColor.r = r + f * (L - r) - 0.3 * abs((gl_FragCoord.y - 500.0) / 500.0);
+    finalColor.g = g + f * (L - g) - 0.3 * abs((gl_FragCoord.y - 500.0) / 500.0);
+    finalColor.b = b + f * (L - b) - 0.3 * abs((gl_FragCoord.y - 500.0) / 500.0);
+    gl_FragColor = finalColor;
   }
 `;
 
