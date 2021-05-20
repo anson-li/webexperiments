287d51a1f04233d015287fcde346d40ecbb4cf0e
Anson Li <hello@ansonli.ca>
2021-05-20 17:06:44 -0600

    Added in description text for both main text and main image
    TODO: Move everything out of main and move to components

--- web-experiments/src/pages/ProjectList/11_SlideInText/index.js
+++ web-experiments/src/pages/ProjectList/11_SlideInText/index.js
@@ -21,6 +21,7 @@ class SlideInText extends PureComponent {
     super(props);
     this.animatedelements = null;
     this.animatedelementsdescription = null;
+    this.childSplit = null;
   }
 
   componentDidMount () {
@@ -32,11 +33,11 @@ class SlideInText extends PureComponent {
 
     // Template for rolling items up into the page
     gsap.set(this.animatedtitle, {
-      perspective: '600px',
+      perspective: '1200px',
       perspectiveOrigin: '300px',
+      scale: 0.75,
       transformOrigin: 'center',
-      scale: 0.5,
-      x: -335,
+      x: -50,
     });
     gsap.set(this.animatedelements.chars, {
       backfaceVisibility: 'hidden',
@@ -48,7 +49,7 @@ class SlideInText extends PureComponent {
       opacity: 0,
       rotationX: '90',
       stagger: 0.1,
-      transformOrigin: '50% 50% 50px',
+      transformOrigin: '50% 50% 100px',
     });
 
     // Template for rolling images up into the page
@@ -73,6 +74,42 @@ class SlideInText extends PureComponent {
       rotationX: '90',
       stagger: 0.1,
       transformOrigin: '50% 50% 100px',
+    });
+
+    // Template for slide up without any rotations
+    const childSplit1 = new SplitText(this.animatedimagedescription, {
+      linesClass: 'inview-split-child',
+      type: 'lines',
+    });
+    // eslint-disable-next-line no-new
+    new SplitText(this.animatedimagedescription, {
+      linesClass: 'inview-split-parent',
+      type: 'lines',
+    });
+    gsap.from(childSplit1.lines, {
+      delay: 1.8,
+      duration: 1.5,
+      ease: 'power4',
+      stagger: 0.1,
+      yPercent: 100,
+    });
+
+    // Template for slide up without any rotations
+    const childSplit2 = new SplitText(this.mainsubtitle, {
+      linesClass: 'inview-split-child',
+      type: 'lines',
+    });
+    // eslint-disable-next-line no-new
+    new SplitText(this.mainsubtitle, {
+      linesClass: 'inview-split-parent',
+      type: 'lines',
+    });
+    gsap.from(childSplit2.lines, {
+      delay: 1.8,
+      duration: 1.5,
+      ease: 'power4',
+      stagger: 0.1,
+      yPercent: 100,
     });
   }
 
@@ -129,29 +166,57 @@ class SlideInText extends PureComponent {
         <div
           className={styles['page-content']}
           style={{backgroundImage: `url(${GridBackground})`}}>
-          <div className={styles['animated-text-template']}>
-            TEXT.
-          </div>
+          {/* <div className={styles['animated-text-template']}>
+            Preon.
+          </div> */}
           <div
-            className={styles['animated-text']}
-            ref={(element) => {
-              this.animatedtitle = element;
-            }}>
-            TEXT.
-          </div>
-          <div
-            className={styles['animated-box']} />
-          <div
-            className={styles['animated-image']}
-            ref={(element) => {
-              this.animatedimage = element;
-            }}>
+            className={styles['main-banner']}>
             <div
-              className={styles['animated-inner-image']}
+              className={styles['animated-text']}
               ref={(element) => {
-                this.animatedinnerimage = element;
+                this.animatedtitle = element;
+              }}>
+              Preon.
+            </div>
+            <div
+              className={styles['main-subtitle']}
+              ref={(element) => {
+                this.mainsubtitle = element;
               }}
-              style={{backgroundImage: 'url(https://unsplash.it/1920/1080?random=1)'}} />
+            >
+              Driven.<br />Design.<br />Perfection.
+            </div>
+          </div>
+          {/* <div
+            className={styles['animated-box']} /> */}
+          <div
+            className={styles['first-image']}>
+            <div
+              className={styles['animated-image']}
+              ref={(element) => {
+                this.animatedimage = element;
+              }}>
+              <div
+                className={styles['animated-inner-image']}
+                ref={(element) => {
+                  this.animatedinnerimage = element;
+                }}
+                style={{backgroundImage: 'url(https://unsplash.it/1920/1080?random=1)'}} />
+            </div>
+            <div
+              className={styles['image-description']}
+              ref={(element) => {
+                this.animatedimagedescription = element;
+              }}>
+              Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy
+              foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive
+              innovation via workplace diversity and empowerment. Bring to the table win-win survival strategies to ensure proactive domination.
+              At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a
+              streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+              Capitalize on low hanging fruit to identify a ballpark value added activity to beta test.
+              Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway
+              will close the loop on focusing solely on the bottom line.
+            </div>
           </div>
         </div>
       </div>
